eb96042bd35b0f529f6dd5d0ac8145eb6a9e4ee0
chen haonan <haonanc1@student.unimelb.edu.au>
2019-03-30 13:19:32 +1100

    no message

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 import os
+from io import BytesIO
 from subprocess import call
 from docker import *
 import tarfile
 import gzip
 import shutil
 import boto3, botocore
-from config import S3_KEY, S3_SECRET, S3_BUCKET
-
-
+from config import S3_KEY, S3_SECRET, S3_BUCKET, S3_LOCATION
+from werkzeug.utils import secure_filename
 
 # Uses wtfforms, bootstrap, jinja
 # python3 -m venv venv
@@ -22,16 +22,13 @@ from config import S3_KEY, S3_SECRET, S3_BUCKET
 # pip install flask-bootstrap
 # pip install docker
 
+# Uses code from http://zabana.me/notes/upload-files-amazon-s3-flask.html
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'rb4(X~f`Y%#"bF8P'
+app.config.from_object("config")
 bootstrap = Bootstrap(app)
 client = from_env()
-
-s3 = boto3.client(
-   "s3",
-   aws_access_key_id=S3_KEY,
-   aws_secret_access_key=S3_SECRET
-)
+s3 = boto3.client("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
 
 # Dictionary that maps the name of the app (key) to a list of required images' names (value)
 # Customise for each use case
@@ -41,14 +38,12 @@ dictionary =  { 'app1' : ['alpine:3.9.2'],
                 'app4' : ['alpine:3.7.3'], 
                 'app5' : ['alpine:3.6.5'],
                 'app6' : ['alpine:3.6'] }
+
 imagesDownloaded=[]
-client = APIClient(base_url='unix://var/run/docker.sock')
 
 
 class SelectForm(FlaskForm):
-    # Choices are in (value1, value2) pairs, 
-    # the first value in the tuple is the one put into a list and stored in form.filesToDownload.data
-    # the second value in the tuple is the one displayed on the web form
+    # Choices are in (value, key) pairs, ideally value would Image ID and key would be Name
     filesToDownload = SelectMultipleField("Choose docker images to download",
                                 choices=[('app1','app1'), ('app2','app2'),('app3','app3'),('app4','app4'),('app5','app5'),('app6','app6')],
                                 validators=[DataRequired()])
@@ -71,48 +66,16 @@ def index():
         session['filesToDownload'] = form.filesToDownload.data
         # Bug: Flash not displaying as wanted, currently only displaying after refresh
         flash("Download will commence shortly")
-        return redirect(url_for('upload_file_to_s3'))
+        return redirect(url_for('download'))
     return render_template('index.html', form=form, filesToDownload=session.get('filesToDownload'))
 
-
-@app.route('/upload_file_to_s3', methods=['GET', 'POST'])
-def upload_file_to_s3(bucket_name = S3_BUCKET, acl="public-read", contentType = "application/tar"):
-          
-    """
-    Docs: http://boto3.readthedocs.io/en/latest/guide/s3.html
-    """
-    apps = session.get('filesToDownload')
-    file = download(apps)
-#    filename = 'download/clientImages.tar'
-    
-    try:
-
-        s3.upload_fileobj(
-            file,
-            bucket_name,
-            S3_KEY,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": contentType,
-                "ContentEncoding": "gzip"
-            }
-        )
-    print("hello*******************")
-    except Exception as e:
-        print("Something Happened: ", e)
-        return e
-
-    return str("{}{}".format(app.config["S3_LOCATION"], S3_KEY))
-
 @app.route('/download', methods=['GET', 'POST'])
-def download(apps):
+def download():
     # Gets data from selectForm and grabs corresponding image objects into a list
     # filesToDownload is a list of strings of image ids
-    
-    glassory = dictionary
-
+    filesToDownload = session.get('filesToDownload')
     filename = 'clientImages.tar'
-
+    # Saves the images into a tar file
     tarfile = open(filename, 'wb')
     for app in apps:
         images = glassory[app]
@@ -132,37 +95,40 @@ def download(apps):
 
     tarfile.close()
     # GZip compresses tar file
-#    with open (filename, 'rb') as f_in, gzip.open(filename+'.gz', 'wb') as f_out:
-
-#        shutil.copyfileobj(f_in, f_out)
-
     compressedFile = BytesIO()
     with open(filename, 'rb') as f_in:
-        with gzip.GzipFile(fileobj = compressedFile, mode = 'wb') as f_out:
+        with gzip.GzipFile(fileobj=compressedFile, mode="wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
-
     compressedFile.seek(0)
 
-    attachment = compressedFile
+    if f_out:
+        output = upload_file_to_s3(compressedFile)
+
     f_in.close()
     f_out.close()
     # Ensures that existings tar files do not get lumped into new download request
     # os.remove should be compatible with all OS
     os.remove(filename)
-#    os.remove(filename+'.gz')
+    
+    return str(output)
 
-    return attachment
+def upload_file_to_s3(file, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            S3_BUCKET,
+            S3_KEY,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": 'application/tar',
+                'ContentEncoding' : 'gzip'
+            }
+        )
+    except Exception as e:
+        # This is a catch all exception, edit this part to fit your needs.
+        print("Something Happened: ", e)
+        return e
+    return "{}{}".format(S3_LOCATION, S3_KEY)
 
-
-
-
-#@app.route('/download', methods=['GET', 'POST'])
-#def get_input_list():
-#    if 'apps' in request.args:
-#        return download(request.args['apps'])
-#    else:
-#        return "Empty or Wrong apps list"
-
-
-
-
+if __name__ == "__main__":
+    app.run()
